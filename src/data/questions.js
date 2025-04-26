5c0fc30f435ca15a899b6716139b7aac5b88edca
veikkapuhalainen <veikka.puhalainen@aalto.fi>
2025-04-26 14:59:42 +0300

    Kysymystiedosto lisätty, kysymysrunko tehty

---
+++ src/data/questions.js
@@ -0,0 +1,54 @@
+const questions = [
+    {
+        question: "Kysymys 1",
+        options: ["1", "2", "3", "4"],
+        correctAnswer: "2"
+    },
+    {
+        question: "Kysymys 2",
+        options: ["5", "6", "7", "8"],
+        correctAnswer: "7"
+    },
+    {
+        question: "Kysymys 3",
+        options: ["9", "10", "11", "12"],
+        correctAnswer: "12"
+    },
+    {
+        question: "Kysymys 4",
+        options: ["1", "2", "3", "4"],
+        correctAnswer: "1"
+    },
+    {
+        question: "Kysymys 5",
+        options: ["5", "6", "7", "8"],
+        correctAnswer: "6"
+    },
+    {
+        question: "Kysymys 6",
+        options: ["9", "10", "11", "12"],
+        correctAnswer: "11"
+    },
+    {
+        question: "Kysymys 7",
+        options: ["1", "2", "3", "4"],
+        correctAnswer: "1"
+    },
+    {
+        question: "Kysymys 8",
+        options: ["5", "6", "7", "8"],
+        correctAnswer: "5"
+    },
+    {
+        question: "Kysymys 9",
+        options: ["9", "10", "11", "12"],
+        correctAnswer: "12"
+    },
+    {
+        question: "Kysymys 10",
+        options: ["1", "2", "3", "4"],
+        correctAnswer: "1"
+    }
+];
+
+export default questions;
