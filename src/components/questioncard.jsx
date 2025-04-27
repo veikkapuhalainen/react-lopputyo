0329dd388535f7bf1d87289b91271aa934f76041
veikkapuhalainen <veikka.puhalainen@aalto.fi>
2025-04-27 21:32:10 +0300

    Vastaaminen kymykseen pitää hyväksyä answer napilla

--- src/components/questioncard.jsx
+++ src/components/questioncard.jsx
@@ -1,24 +1,52 @@
-import './questioncard.css'
+import "./questioncard.css";
 
-function QuestionCard({questionNumber, question, options, points, answerSelected}) {
-    return (
-        <div className="question-card">
-            <div className='question-card-header'>
-                <div className='question-number'>
-                    Question #{questionNumber}
-                </div>
-                <div className="question-text">{question}</div>
-            </div>
-            <div className='points'>Points {points}</div>
-            <div className="option-list">
-                {options.map((option, index) => (
-                    <button className="option-item" key={index} onClick={ () => answerSelected(option)}>
-                        {option}
-                    </button>
-                ))}
-            </div>
-        </div>
-    );
+function QuestionCard({
+  questionNumber,
+  question,
+  options,
+  selectedOption,
+  points,
+  onSelect,
+  answer,
+  endQuiz,
+}) {
+  return (
+    <div className="question-card">
+      <div className="question-card-header">
+        <div className="question-number">Question #{questionNumber}</div>
+        <div className="question-text">{question}</div>
+      </div>
+      <div className="points">Points {points}</div>
+      <div className="option-list">
+        {options.map((option, index) => (
+          <button
+            className={
+              selectedOption === option ? "selected-option-item" : "option-item"
+            }
+            key={index}
+            onClick={() => onSelect(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+
+      <div className="bottom-buttons">
+        <button
+          className="end-quiz-btn"
+          children={"End Quiz"}
+          onClick={endQuiz}
+        />
+        {selectedOption && (
+        <button
+            className="answer-quiz-btn"
+            children={"Answer"}
+            onClick={answer}
+            />
+      )}
+      </div>
+    </div>
+  );
 }
 
 export default QuestionCard;
--- src/pages/quizpage.jsx
+++ src/pages/quizpage.jsx
@@ -4,7 +4,7 @@ import questions from "../data/questions";
 import { useEffect, useState } from "react";
 import QuestionCard from "../components/questioncard";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 // Kysymys sivun luominen
 function QuizPage() {
@@ -19,6 +19,7 @@ function QuizPage() {
   const [points, setPoints] = useState(0);
   const [questionIndex, setQuestionIndex] = useState(0);
   const [quizFinished, setQuizFinished] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
 
   // Haetaan alussa kysymykset data/questions/, sekoitetaan ne ja asetetaan 10 ensimmäistä useStatelle
   useEffect(() => {
@@ -27,54 +28,60 @@ function QuizPage() {
     setQuestions(tenQuestions);
   }, []);
 
-  function checkAnswer(selectedOption) {
+  function markSelectedOption(option) {
+    setSelectedOption(option);
+  }
+
+  function checkAnswer() {
     const currentQuestion = selectedQuestions[questionIndex];
 
     if (selectedOption === currentQuestion.correctAnswer) {
       setPoints(points + 1);
-      toast.success('Oikein!');
+      toast.success("Oikein!");
     } else {
-        toast.error('Väärin');
+      toast.error("Väärin");
     }
+
 
     if (questionIndex + 1 < selectedQuestions.length) {
       setQuestionIndex(questionIndex + 1);
+      setSelectedOption(null);
     } else {
       setQuizFinished(true);
-      }
     }
+  }
 
-    if (quizFinished) {
-        return (
-            <>
-                <h1>Tähän joku komponentti, sait {points} pistettä!</h1>;
-                <Button children={"End Quiz"} onClick={endQuiz} />
-            </>
-        )
-    }
-
-    if (selectedQuestions.length === 0) {
-        return <div>Ladataan kysymyksiä...</div>;
-    }
-
+  if (quizFinished) {
     return (
-        <>
-            <div className="quiz-page">
-                <QuestionCard
-                questionNumber={questionIndex + 1}
-                question={selectedQuestions[questionIndex].question}
-                options={selectedQuestions[questionIndex].options}
-                points={points}
-                answerSelected={checkAnswer}
-                />
-                <ToastContainer
-                    autoClose={1500}
-                    pauseOnHover={false} />
-                <button className="end-quiz-btn" children={"End Quiz"} onClick={endQuiz} />
-            </div>
-        </>
+      <>
+        <h1>Tähän joku komponentti, sait {points} pistettä!</h1>;
+        <button children={"End Quiz"} onClick={endQuiz} />
+      </>
     );
-    }
+  }
 
+  if (selectedQuestions.length === 0) {
+    return <div>Ladataan kysymyksiä...</div>;
+  }
+
+  return (
+    <>
+      <div className="quiz-page">
+        <h1>Quiz-Game</h1>
+        <QuestionCard
+          questionNumber={questionIndex + 1}
+          question={selectedQuestions[questionIndex].question}
+          options={selectedQuestions[questionIndex].options}
+          selectedOption={selectedOption}
+          points={points}
+          onSelect={markSelectedOption}
+          answer={checkAnswer}
+          endQuiz={endQuiz}
+        />
+        <ToastContainer autoClose={1500} pauseOnHover={false} position="top-center" />
+      </div>
+    </>
+  );
+}
 
 export default QuizPage;
